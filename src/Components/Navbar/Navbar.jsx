95c3f31aa29eae796ced228bc9a8d8b834297dfe
Judah Abraham <judahabrahamforgood@gmail.com>
2025-06-28 22:07:21 +0100

    Updates

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import { useState , useEffect } from 'react'
+import {  TbMenuDeep } from "react-icons/tb";
 const Navbar = () => {
   const[scrolled , setScrolled] = useState(false);
   useEffect(()=>{
@@ -15,6 +16,7 @@ const Navbar = () => {
     <nav className={`nav ${scrolled ? 'nav-scroll' : ''}`}>
       <h1 className=' nav-header'>Dev<span className="dev">Chuks</span></h1>
       <ul className="nav-links">
+        <TbMenuDeep color='white' size={25}/>
         <li className='HideonMobile'><a href="#about">About</a></li>
         <li className='HideonMobile'><a href='#skills'> Skills</a></li>
         <li className='HideonMobile'><a href="#experience">Experience</a></li>
